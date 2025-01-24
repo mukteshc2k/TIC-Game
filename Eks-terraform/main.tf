aa60b5e676caa209374a39f301018019ecbc3c84
mukteshc2k <91601527+mukteshc2k@users.noreply.github.com>
2025-01-24 12:24:35 +0000

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -1,3 +1,111 @@
+# Create an OIDC identity provider for EKS
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.tic_game_cluster.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.tic_game_cluster.name
+}
+
+resource "aws_iam_openid_connect_provider" "karpenter_oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.aws_eks_cluster.cluster.identity[0].oidc.issuer_thumbprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc.issuer
+}
+
+# Create an IAM Role for Karpenter controller
+data "aws_iam_policy_document" "karpenter_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.karpenter_oidc_provider.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_eks_cluster.cluster.identity[0].oidc.issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:karpenter:karpenter"]
+    }
+  }
+}
+
+resource "aws_iam_role" "karpenter_controller_role" {
+  name               = "karpenter-controller-role"
+  assume_role_policy = data.aws_iam_policy_document.karpenter_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_controller_policy" {
+  role       = aws_iam_role.karpenter_controller_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterAutoscalerPolicy"
+}
+
+# Install Karpenter Helm chart
+resource "helm_release" "karpenter" {
+  name       = "karpenter"
+  namespace  = "karpenter"
+  chart      = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  version    = "v0.29.0" # Update to the latest stable version
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "karpenter"
+  }
+
+  set {
+    name  = "controller.clusterName"
+    value = aws_eks_cluster.tic_game_cluster.name
+  }
+
+  set {
+    name  = "controller.clusterEndpoint"
+    value = data.aws_eks_cluster.cluster.endpoint
+  }
+
+  set {
+    name  = "controller.aws.defaultInstanceProfile"
+    value = "karpenter-node-instance-profile" # Replace with your node instance profile
+  }
+}
+
+# Create a Karpenter provisioner (YAML example)
+resource "kubernetes_manifest" "karpenter_provisioner" {
+  manifest = <<EOT
+apiVersion: karpenter.sh/v1alpha5
+kind: Provisioner
+metadata:
+  name: default
+spec:
+  requirements:
+    - key: "karpenter.sh/capacity-type"
+      operator: "In"
+      values: ["on-demand", "spot"]
+    - key: "node.kubernetes.io/instance-type"
+      operator: "In"
+      values: ["t3.medium", "t3.large"]
+  limits:
+    resources:
+      cpu: 1000
+      memory: 2000Gi
+  provider:
+    cluster:
+      endpoint: "${data.aws_eks_cluster.cluster.endpoint}"
+      name: "${aws_eks_cluster.tic_game_cluster.name}"
+EOT
+}
+
+##############################################################
+
+
+
+
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
